804ace726f26324899889e76961507c7bbff54f4
antonio_dp <trovao10000@hotmail.com>
2025-02-25 22:19:10 -0300

    Criação/pratica com Pilhas

--- exerciciosJava/src/pilhas/PilhaTest.java
+++ exerciciosJava/src/pilhas/PilhaTest.java
@@ -3,13 +3,17 @@ package pilhas;
 public class PilhaTest {
     public static void main(String[] args) {
         Livro livro = new Livro("título do livro", 300);
+        Livro livro2 = new Livro("título do livro 2", 200);
         Pilha pilha = new Pilha();
 
         pilha.empilhar(livro);
+        pilha.empilhar(livro2);
         System.out.println(pilha.estaVazia()); // false
         System.out.println(pilha.imprimeTodosLivros()); // título do livro
         System.out.println(pilha.espiaTopo()); // título do livro
-        Livro livroDesenpilhado = pilha.desenpilhar();
-
+        pilha.desenpilhar();
+        pilha.desenpilhar();
+        System.out.println(pilha.estaVazia()); // true
+        pilha.imprimeTodosLivros();
     }
 }
